0375e1fdde198e3736d8bef5f6fc9a4ef5a4257e
TaeksuKim <taeka89@gmail.com>
2017-03-21 11:47:07 +0900

    update property

--- app/src/main/java/com/taeksukim/android/sharedpreference/MainActivity.java
+++ app/src/main/java/com/taeksukim/android/sharedpreference/MainActivity.java
@@ -51,42 +51,4 @@ public class MainActivity extends AppCompatActivity {
         layout.setVisibility(View.GONE);
         propertyUtil.saveProperty("firstOpen", "false");
     }
-
-
-    public void saveProperty(String key, String value){
-        Properties prop = new Properties();
-        prop.put(key, value);
-
-
-        try {
-            //앱의 내부 저장소/ files/test.properties 파일을 저장
-            FileOutputStream fos = new FileOutputStream(internalStorePath + "/" + propertyFile);
-
-            prop.store(fos, "코멘트테스트");//firstOpen= false
-
-            // 저장 후 파일을 닫아준다.
-            fos.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public String getProperty(String key){
-        String value = "";
-
-        Properties prop = new Properties();
-
-        try {
-            FileInputStream fis = new FileInputStream(internalStorePath + "/" + propertyFile);
-            prop.load(fis);
-            fis.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        prop.list(System.out);// 프로퍼티 목록 전체 나열하기
-
-        value = prop.getProperty(key);
-        return value;
-    }
 }
